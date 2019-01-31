c0b5253854ed9250ab4103b2f287a66443592b21
Waterstrong <sqlin@thoughtworks.com>
2019-02-01 09:13:11 +1100

    [Shuiqiang] - remove the pact code for TODO

--- consumer-blue/src/test/java/au/com/pact/demo/service/AddressServiceImplPactTest.java
+++ consumer-blue/src/test/java/au/com/pact/demo/service/AddressServiceImplPactTest.java
@@ -32,38 +32,12 @@ public class AddressServiceImplPactTest {
     private RestTemplateExecutor restTemplate = buildRestTemplate("http://localhost:8082");
 
     // TODO: Create rule PactProviderRuleMk2 to enable mock provider
-    @Rule
-    public PactProviderRuleMk2 mockProvider = new PactProviderRuleMk2(PROVIDER_LEMON, "localhost", 8082, this);
+
 
     // TODO: Create Address Pact (Provider Lemon <--> Consumer Blue)
-    @Pact(provider = PROVIDER_LEMON, consumer = CONSUMER_BLUE)
-    public RequestResponsePact createAddressPact(PactDslWithProvider builder) {
-        PactDslJsonBody expectedResponse = new PactDslJsonBody()
-                .array("addresses")
-                .stringType("1304/7 Riverside Quay, VIC 3006")
-                .stringType("1305/8 Riverside Quay, VIC 3006")
-                .stringType("1306/9 Riverside Quay, VIC 3006")
-                .closeArray()
-                .asBody();
 
-        return builder
-                .uponReceiving("Search Addresses")
-                .path("/addresses")
-                .query("keyword=13 Riverside")
-                .method("GET")
-                .willRespondWith()
-                .status(200)
-                .body(expectedResponse)
-                .toPact();
-    }
 
     // TODO: Create Pact Test for Provider Lemon
-    @Test
-    @PactVerification(PROVIDER_LEMON)
-    public void shouldSearchAddressesGivenAddressKeyword() {
-        AddressResponse addressResponse = addressService.searchAddresses("13 Riverside");
-        assertThat(addressResponse.getAddresses(), hasSize(3));
-        assertThat(addressResponse.getAddresses().get(0), is("1304/7 Riverside Quay, VIC 3006"));
-    }
+
 
 }
--- consumer-blue/src/test/java/au/com/pact/demo/service/DecisionServiceImplPactTest.java
+++ consumer-blue/src/test/java/au/com/pact/demo/service/DecisionServiceImplPactTest.java
@@ -32,49 +32,12 @@ public class DecisionServiceImplPactTest {
     private RestTemplateExecutor restTemplate = buildRestTemplate("http://localhost:8081");
 
     // TODO: Create rule PactProviderRuleMk2 to enable mock provider
-    @Rule
-    public PactProviderRuleMk2 mockProvider = new PactProviderRuleMk2(PROVIDER_APPLE, "localhost", 8081, this);
+
 
     // TODO: Create Decision Pact (Provider Apple <--> Consumer Blue)
-    @Pact(provider = PROVIDER_APPLE, consumer = CONSUMER_BLUE)
-    public RequestResponsePact createRejectedDecisionPact(PactDslWithProvider builder) {
-        PactDslJsonBody rejectedResponse = new PactDslJsonBody()
-                .stringType("decision", "Rejected")
-                .stringType("policyRule", "PR-8")
-                .stringType("credRule", "CR-6")
-                .booleanType("flag", true)
-                .asBody();
 
-        PactDslJsonBody rejectedRequest = new PactDslJsonBody()
-                .stringType("profileId")
-                .stringValue("transaction", "no")
-                .asBody();
-
-        return builder
-                .given("Test Rejected")
-                .uponReceiving("Process Decision")
-                .path("/decision")
-                .body(rejectedRequest)
-                .method("POST")
-                .willRespondWith()
-                .status(200)
-                .body(rejectedResponse)
-                .toPact();
-    }
 
     // TODO: Create Pact Test for Provider Apple
-    @Test
-    @PactVerification(PROVIDER_APPLE)
-    public void shouldProcessDecisionToReturnRejectedGivenDecisionRequestTransactionNo() {
-        DecisionRequest request = new DecisionRequest();
-        request.setProfileId("id12345");
-        request.setTransaction("no");
 
-        DecisionResponse response = decisionService.processDecision(request);
 
-        assertThat(response.getDecision(), is("Rejected"));
-        assertThat(response.getPolicyRule(), is("PR-8"));
-        assertThat(response.getCredRule(), is("CR-6"));
-        assertThat(response.isFlag(), is(true));
-    }
 }
